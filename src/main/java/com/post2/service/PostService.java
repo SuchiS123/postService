e574a8b52a1b8bef22f17bd9c2e521c322b6df72
Your Name <your.email@example.com>
2025-03-09 00:28:42 +0530

    \not notttttt

--- src/main/java/com/post2/service/PostService.java
+++ src/main/java/com/post2/service/PostService.java
@@ -87,7 +87,7 @@ public class PostService {
         ArrayList forObject = restTemplate.getForObject("http://localhost:8082/api/comments/" + postId, ArrayList.class);
 
         Post postById = postRepository.findById(postId).orElseThrow(()-> new ResourceNotFound(
-                "Post Not Found"));
+                "Post Nottttttt Found"));
 
         PostDto postDto = new PostDto();
         postDto.setId(postById.getId());
